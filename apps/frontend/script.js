de58674f6a3eeaa060233a313936dd3b2c624c44
Anton Yashin <antoshkoo@gmail.com>
2025-05-21 15:00:01 +0800

    fix: update request route

--- apps/frontend/script.js
+++ apps/frontend/script.js
@@ -1,7 +1,7 @@
 console.log("Hello World!", window.origin);
 
 const request = () => {
-  const url = "https://dev-era-vr.ru/api/";
+  const url = "https://api.dev-era-vr.ru/";
   const options = {
     method: "GET",
   };
