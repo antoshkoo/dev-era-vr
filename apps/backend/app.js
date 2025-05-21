b2873704603cc5563ae4242c9a524cfa21c93478
Anton Yashin <antoshkoo@gmail.com>
2025-05-21 13:44:45 +0800

    feat: add cors

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -6,13 +6,7 @@ const port = 3000;
 app.get(
   "/",
   cors({
-    origin: [
-      "http://127.0.0.1",
-      "http://localhost",
-      "http://91.218.143.75",
-      "http://www.dev-era-vr.ru",
-      "http://dev-era-vr.ru",
-    ],
+    origin: ["http://localhost", "https://dev-era-vr.ru"],
   }),
   (req, res) => {
     res.send({ message: "Hello World!!!" });
